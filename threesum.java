eb439946e1b6665453fc0a00cf10cfa7a5f830bd
Saksham Sharma <118747569+sak-x-sham@users.noreply.github.com>
2023-11-03 15:51:17 +0530

    Add files via upload

---
+++ threesum.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;  
+            int right = nums.length - 1;  
+
+            while (left < right) {
+                int total = nums[i] + nums[left] + nums[right];
+
+                if (total == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    while (left < right && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+
+                    left++;
+                    right--;
+                } else if (total < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum solution = new ThreeSum();
+        int[] nums = {1, 1, 1, 1, 1}; 
+        List<List<Integer>> result = solution.threeSum(nums);
+
+        for (List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+}
