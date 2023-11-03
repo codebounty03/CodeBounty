35bb981a09bff6e287f7af3a146d11682553e2e2
Fahad kirmani <118795629+fahad-kirmani@users.noreply.github.com>
2023-11-03 15:30:52 +0530

    Add files via upload

---
+++ Median_of_Two_Sorted_Arrays.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def findMedianSortedArrays(self, nums1, nums2):
+        merged_list = nums1 + nums2
+        merged_list.sort(reverse=True)  
+        total_elements = len(merged_list)
+
+        if total_elements % 2 == 1:
+            return float(merged_list[total_elements // 2])
+        else:
+            middle1 = merged_list[total_elements // 2 ]
+            middle2 = merged_list[total_elements // 2 - 1]
+            return (middle1+middle2)/2
+        
+
+solution = Solution()
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+result = solution.findMedianSortedArrays(nums1, nums2)
+
+print("Median:", result)
