26bcc162d455441f99db7586f6ec6679f8b1b18c
Meet Thakur <99238677+MeetThakur@users.noreply.github.com>
2023-11-03 15:46:21 +0530

    Update Intersection_of_linked_lists.py

--- Python/Intersection_of_linked_lists.py
+++ Python/Intersection_of_linked_lists.py
@@ -1,28 +1,23 @@
 class Solution:
-    def length(self, x):
-        l = 0
-        while x:
-            l += 1
-            x = x.next
-        return l
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        stackA = ['A']
+        stackB = ['B']
 
-    def getIntersectionNode(self, A, B):
-        a = A
-        b = B
-        la = self.length(a)
-        lb = self.length(b)
-        while a and b:
-            if la > lb:
-                la -= 1
-                a = a.next
-            elif la < lb:
-                lb -= 1
-                b = b.next
-            else:
-                if a == b:
-                    return a
-                a = a.next
-                b = b.next
-                la -= 1
-                lb -= 1
-        return None
+        while headA or headB:
+            if headA:
+                stackA.append(headA)
+                headA = headA.next
+
+            if headB:
+                stackB.append(headB)
+                headB = headB.next
+
+        prev = None
+        while stackA and stackB:
+            nodeA = stackA.pop(-1)
+            nodeB = stackB.pop(-1)
+
+            if nodeA != nodeB:
+                return prev
+
+            prev = nodeA
